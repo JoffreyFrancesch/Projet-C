15bc557cf04b8360ed8a7371357d60da56b4c971
JoffreyFrancesch <franceschini@et.esiea.fr>
2017-10-25 10:31:54 +0200

    Tram 3 Wagon affichage OK

--- Zone de TEST/tram.c
+++ Zone de TEST/tram.c
@@ -37,18 +37,46 @@ void affiche_map(){
 ////////////////////////
 void tram1(int pos_l1){
   if (pos_l1 <= 43) {
-    printf("\033[%d;30H█\n",pos_l1);
+    printf("\033[0;36m\033[%d;30H█\033[0m\n",pos_l1);
+    if (pos_l1 >= 7) {
+      printf("\033[0;36m\033[%d;30H█\033[0m\n",pos_l1-1);
+    }
+    if (pos_l1 >= 8){
+      printf("\033[0;36m\033[%d;30H█\033[0m\n",pos_l1-2);
+    }
   }
-  if (pos_l1-1 >= 6 && pos_l1-1 <= 43){
-    printf("\033[%d;30H \n",pos_l1-1);
+  if (pos_l1-3 >= 6 && pos_l1-3 <= 43){
+    printf("\033[%d;30H \n",pos_l1-3);
+  }
+  if (pos_l1-2 == 41) {
+    printf("\033[41;30H \n");
+    printf("\033[0;36m\033[42;30H█\033[0m\n");
+  }
+  if (pos_l1-1 == 42) {
+    printf("\033[42;30H \n");
+    printf("\033[0;36m\033[43;30H█\033[0m\n");
   }
 }
 void tram2(int pos_l2){
   if (pos_l2 >= 6) {
-    printf("\033[%d;165H█\n",pos_l2);
+    printf("\033[0;36m\033[%d;165H█\033[0m\n",pos_l2);
+    if (pos_l2 <= 42) {
+      printf("\033[0;36m\033[%d;165H█\033[0m\n",pos_l2+1);
+    }
+    if (pos_l2 <= 41) {
+      printf("\033[0;36m\033[%d;165H█\033[0m\n",pos_l2+2);
+    }
   }
-  if (pos_l2+1 <= 43 && pos_l2+1 >= 6){
-    printf("\033[%d;165H \n",pos_l2+1);
+  if (pos_l2+3 <= 43 && pos_l2+3 >= 6){
+    printf("\033[%d;165H \n",pos_l2+3);
+  }
+  if (pos_l2+2 == 8) {
+    printf("\033[8;165H \n");
+    printf("\033[0;36m\033[7;165H█\033[0m\n");
+  }
+  if (pos_l2+1 == 7) {
+    printf("\033[7;165H \n");
+    printf("\033[0;36m\033[6;165H█\033[0m\n");
   }
 }
 ////////////////////////
@@ -69,9 +97,9 @@ int main(int argc, char  *argv[]) {
       pos_l2 = 43;
       printf("\033[43;30H \033[6;165H \n");
     }
-    if (pos_l1 == 14 || pos_l2 == 12) {
+  /*if (pos_l1 == 14 || pos_l2 == 12) {
       usleep(500000);
-    }
+    }*/
   }
 
 
