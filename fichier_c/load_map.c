da5ce1844639422fc494eb8b3b076896e846c80f
JoffreyFrancesch <franceschini@et.esiea.fr>
2017-11-07 17:09:47 +0100

    Ajout fonction Key_pressed

--- fichier_c/load_map.c
+++ fichier_c/load_map.c
@@ -51,3 +51,30 @@ void affiche_map(){
   printf("\n");
   fclose(map);
 }
+
+char key_pressed() {
+	struct termios oldterm, newterm;
+	int oldfd;
+	char c, result = 0;
+
+	tcgetattr (STDIN_FILENO, &oldterm);
+	newterm = oldterm;
+	newterm.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr (STDIN_FILENO, TCSANOW, &newterm);
+	oldfd = fcntl(STDIN_FILENO, F_GETFL, 0);
+	fcntl (STDIN_FILENO, F_SETFL, oldfd | O_NONBLOCK);
+	c = getchar();
+	tcsetattr (STDIN_FILENO, TCSANOW, &oldterm);
+	fcntl (STDIN_FILENO, F_SETFL, oldfd);
+
+	if (c != EOF) {
+		ungetc(c, stdin);
+		result = getchar();
+	}
+	return result;
+}
+/*void temps(double seconde){
+    seconde*=1000000;
+    fflush(stdout);
+    usleep(seconde);
+}*/
